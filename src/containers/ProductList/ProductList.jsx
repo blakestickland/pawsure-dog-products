4d190f703579128a955325201dfd86f8f2dbe800
Blake Stickland <blakestickland@gmail.com>
2021-10-20 21:58:52 +1100

    firebase db created and now pulling data from it to populate the cards

--- src/containers/ProductList/ProductList.jsx
+++ src/containers/ProductList/ProductList.jsx
@@ -1,11 +1,14 @@
 import styles from './ProductList.module.scss';
-import products from "../../services/products";
+// import products from "../../services/products";
 import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getProducts } from "../../services/products";
 
-const useQuery = () => {
-  const location = useLocation();
-  return new URLSearchParams(location.search);
-};
+
+// const useQuery = () => {
+//   const location = useLocation();
+//   return new URLSearchParams(location.search);
+// };
 
 const ProductCard = ({ product }) => {
   return (
@@ -22,23 +25,42 @@ const ProductCard = ({ product }) => {
 };
 
 const ProductList = () => {
-    const query = useQuery();
-    const name = query.get("name") ?? "";
-    const limit = query.get("limit")
-        ? parseInt(query.get("limit"))
-        : 10;
+    
+    // const query = useQuery();
+    // const name = query.get("name") ?? "";
+    // const limit = query.get("limit")
+    //     ? parseInt(query.get("limit"))
+    //     : 10;
+    
+    // const filteredProducts = products.filter((product) => {
+        //     return `${product.productName}
+        //         ${product.productType}`
+        //         .includes(name);
+        // }).slice(0, limit);
 
-    const filteredProducts = products.filter((product) => {
-        return `${product.productName}
-            ${product.productType}`
-            .includes(name);
-    }).slice(0, limit);
+        const [products, setProducts] = useState(null);
+        
+        const populateProducts = async () => {
+            const data = await getProducts();
+            setProducts(data);
+        };
+
+        useEffect(() => {
+            populateProducts();
+        }, []);
 
     return (
       <div className={styles.ProductList}>
-        {filteredProducts.map((product, index) => (
+        {/* {filteredProducts.map((product, index) => (
           <ProductCard product={product} key={index} />
-        ))}
+        ))} */}
+        {products && 
+        products.map((product, index) => (
+            <ProductCard 
+                product={product}
+                key={index}
+            />
+        ))};
       </div>
     );
 };
