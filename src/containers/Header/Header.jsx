1741575fc208eb134b46e54cc03cf8499ea03dea
Blake Stickland <blakestickland@gmail.com>
2021-10-20 18:09:31 +1100

    initial setup of routes and takes user to home, products and individual product

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,13 @@
+import Nav from "../../components/Nav";
+
+const Header = () => {
+    return (
+      <div>
+        <h1>Pawsure</h1>
+        <h2>-- products for the perfect pooch --</h2>
+        <Nav />
+      </div>
+    );
+};
+
+export default Header;
