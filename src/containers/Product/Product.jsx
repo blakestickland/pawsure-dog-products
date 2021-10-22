38b855b722e8757459815ef24497bd07f519aa2b
Blake Stickland <blakestickland@gmail.com>
2021-10-22 20:19:18 +1100

    images stored in Storage and now refernced in .image field and load on Product page

--- src/containers/Product/Product.jsx
+++ src/containers/Product/Product.jsx
@@ -22,15 +22,27 @@ const Product = () => {
     if (!product) {
         return <h2>Product with Id: { id } not found.</h2>
     };
+    // let prepImage = product.image.slice(5);
+    // let image = 
+    //     "https://firebasestorage.googleapis.com/v0/b/" +
+    //     prepImage +
+    //     product.mediaToken;
 
   return (
     <div className={styles}>
-        <h2>
-            {product.productName} [{product.productType}]
-        </h2>
-        <p>Size: {product.size}</p>
-        <p>Price: ${product.price}</p>
-        <p>Type: {product.productType}</p>
+      <h2>
+        {product.productName} [{product.productType}]
+      </h2>
+      <img
+        // href="https://firebasestorage.googleapis.com/v0/b/pawsure-firestore.appspot.com/o/images%2Fchicken-jerky-100g.JPG?alt=media&token=9ebd8ab2-2b83-4637-8027-5cda5ffd38c6"
+        src={product.image}
+        alt={product.productName}
+        width="400"
+        height="300"
+      />
+      <p>Size: {product.size}</p>
+      <p>Price: ${product.price}</p>
+      <p>Type: {product.productType}</p>
     </div>
   );
 };
