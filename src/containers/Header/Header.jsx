8a8faf8c70b86a98503352222b6dcc745e9b3d94
Blake Stickland <blakestickland@gmail.com>
2021-10-29 20:40:24 +1100

    used react bootstrap sytling for responsive navbar that positions well on all screen sizes

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,19 +1,34 @@
 import Navigation from "../../components/Nav";
 import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import  Container  from "react-bootstrap/Container";
+
 
 const Header = ({ countCartItems }) => {
     return (
+      <Container>
+
       <div className={styles.Header}>
-        <img src="https://firebasestorage.googleapis.com/v0/b/pawsure-firestore.appspot.com/o/images%2Fpawsure_logo.jpeg?alt=media&token=43254094-2228-44e1-9bfb-10acb024d882" alt="Pawsure company logo" />
-        <Navigation />
-        <div>
-          Cart {' '}
-          {countCartItems
-            ? (<button className="badge">{countCartItems}</button>)
-            : ('')
-          }
+        <Link to="/">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/pawsure-firestore.appspot.com/o/images%2Fpawsure_logo.jpeg?alt=media&token=43254094-2228-44e1-9bfb-10acb024d882"
+            alt="Pawsure company logo"
+            className={styles.Header__img}
+          />
+        </Link>
+        <div className={styles.Header__Navigation}>
+          <Navigation />
+        </div>
+        <div className={styles.Header__cartDiv}>
+          Cart{" "}
+          {countCartItems ? (
+            <button className="badge">{countCartItems}</button>
+          ) : (
+            ""
+          )}
         </div>
       </div>
+      </Container>
     );
 };
 
